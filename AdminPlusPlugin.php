064cf19db858ba19acbb4f66ad3ceb7473894b71
Sara Palmer <sara.palmer@emory.edu>
2019-03-13 13:40:06 -0400

    delete old code

--- AdminPlusPlugin.php
+++ AdminPlusPlugin.php
@@ -19,13 +19,6 @@ class AdminPlusPlugin extends Omeka_Plugin_AbstractPlugin
 		// ... but are able to publish their own items
 		$acl->allow('adminplus', array('Appearance', 'Themes'));
 
-
-    //     // EDITORS inherit the rights of Authors...
-		// $acl->addRole(new Zend_Acl_Role('editor'), 'author');
-		// // ... but are able to edit and delete Items and Files created by other users and make items Featured
-		// $acl->allow('editor','Items',array('makeFeatured','edit','delete'));
-		// $acl->allow('editor','Files',array('edit','delete'));
-
     }
 
     public function hookAdminUsersForm($args){
